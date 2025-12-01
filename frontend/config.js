acba25bfc0cfd1d97bd6788444d0f012d3988a83
takedaxz <datas.pookan@gmail.com>
2025-12-01 06:51:14 +0000

    Update BASE_URL in config.js to new Cloudflare tunnel URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // config.js - Update BASE_URL with your current Cloudflare tunnel URL
-const BASE_URL = 'https://pen-advantages-possibly-pollution.trycloudflare.com';
+const BASE_URL = 'https://drill-marker-wit-plaza.trycloudflare.com';
