14f1ebe602ee16748c77c3b4ec9943b597f756f1
takedaxz <datas.pookan@gmail.com>
2025-12-03 04:14:56 +0000

    Update BASE_URL in config.js to new Cloudflare tunnel URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // config.js - Update BASE_URL with your current Cloudflare tunnel URL
-const BASE_URL = 'https://blackberry-red-launch-concentrations.trycloudflare.com';
+const BASE_URL = 'https://forty-filme-better-semi.trycloudflare.com';
 //const BASE_URL = 'localhost:5000';
